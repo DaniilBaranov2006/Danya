d1ca70d7e1837e1dee173aea05b2fe7c28397731
DaniilBaranov2006 <DaniilBaranov2006@gmail.com>
2019-06-06 20:11:09 +0300

    Ответы 2задача

--- answer/2.py
+++ answer/2.py
@@ -1,11 +1,22 @@
 from turtle import *
 
+speed(0)
+
 for i in range(180):
 
-	forward(100)
-	right(90)
-	forward(50)
-	right(90)
+	forward(140)
+	right(70)
+	forward(70)
+	left(90)
+	forward(90)
+	penup()
+	goto(0,0)
+	pendown()
+	right(18)
 
-	forward(150)
-	exitonclick()
+exitonclick()
+
+
+
+
+
