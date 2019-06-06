eb46003582044eea5dd77b8fe76aedf599b2f7a9
DaniilBaranov2006 <DaniilBaranov2006@gmail.com>
2019-06-06 19:41:47 +0300

    sdf

---
+++ answer/1.py
@@ -0,0 +1,10 @@
+from turtle import *
+
+for i in range(180):
+
+
+fd(50)
+left(120)
+fd(50)
+left(120)
+fd(50)
---
+++ answer/2.py
@@ -0,0 +1,11 @@
+from turtle import *
+
+for i in range(180):
+
+	forward(100)
+	right(90)
+	forward(50)
+	right(90)
+
+	forward(150)
+	exitonclick()
